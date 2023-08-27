7c88833d033ae64cd5829ca2982d6b376601411b
UNKNOWN <79321850+Jatindalal875@users.noreply.github.com>
2023-08-28 00:43:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,17 +8,18 @@ load_dotenv()
 class Config(object):
     LOGGER = True
 
-    API_ID = getenv("API_ID", None)
-    API_HASH = getenv("API_HASH", None)
+    API_ID = getenv("API_ID", "27175096")
+    API_HASH = getenv("API_HASH", "22930f3501fc1c277e707d385c772547
+Api hash")
     ARQ_API_KEY = "PMPTTD-HOMLMF-SRBHNH-RZMWXL-ARQ"
     SPAMWATCH_API = None
-    TOKEN = getenv("TOKEN", None)
+    TOKEN = getenv("TOKEN", "6560530370:AAGbw_AIz_QQaEETuu2hA4U3DDXcysV-kAk")
     OWNER_ID = int(getenv("OWNER_ID", 5938660179))
-    OWNER_USERNAME = getenv("OWNER_USERNAME", "Abishnoi1M")
-    SUPPORT_CHAT = getenv("SUPPORT_CHAT", "chatting_hub51")
-    LOGGER_ID = int(getenv("LOGGER_ID", "-1001819078701"))
-    MONGO_URI = getenv("MONGO_DB_URI", None)
-    DB_NAME = getenv("DB_NAME", "ExonRobot")
+    OWNER_USERNAME = getenv("OWNER_USERNAME", "anshull_1")
+    SUPPORT_CHAT = getenv("SUPPORT_CHAT", "shoko_support")
+    LOGGER_ID = int(getenv("LOGGER_ID", "-1001980710142"))
+    MONGO_URI = getenv("MONGO_DB_URI", "mongodb+srv://anshul:swayam@cluster0.jiphfti.mongodb.net/?retryWrites=true&w=majority")
+    DB_NAME = getenv("DB_NAME", "Komi")
     REDIS_URL = "redis://default:wK6ZCiclq4iQKYpgfY90v6kd6WdPfEwl@redis-10186.c263.us-east-1-2.ec2.cloud.redislabs.com:10186/default"
     DATABASE_URL = getenv("DATABASE_URL", None)
 
